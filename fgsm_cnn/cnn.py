9de04f317b616980c0cc20d4576fd1e4b23a9813
Caules Ge <caules.ge@mail.utoronto.ca>
2026-04-04 03:35:33 -0400

    added maxout network for CNN

--- fgsm_cnn/cnn.py
+++ fgsm_cnn/cnn.py
@@ -27,3 +27,32 @@ net = Net()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+
+class MaxoutNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        
+        # Maxout for FC1: We want 120 outputs, so we compute 240 linear values
+        self.fc1_linear = nn.Linear(16 * 4 * 4, 120 * 2) 
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        
+        # --- Maxout implementation ---
+        x = self.fc1_linear(x)
+        # Reshape to (batch, 120, 2)
+        x = x.view(-1, 120, 2)
+        # Take the max along the last dimension (the 2 linear pieces)
+        x, _ = torch.max(x, dim=2)
+        # -----------------------------
+        
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
